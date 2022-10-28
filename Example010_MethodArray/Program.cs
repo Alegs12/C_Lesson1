cf03e01381378939dbdc965013117e2265279e93
Alexandr <algran83@mail.ru>
2022-10-28 15:28:54 +0300

    Add Ex010

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array = {1, 2, 3, 4, 6, 4, 5, 66};
+int n = array.Length;
+int find = 4;
+int index = 0;
+
+while (index < n)
+    {
+    if (array[index] == find)
+        {
+        Console.WriteLine("Найдено! " + "Индекс = " + index);
+        break;
+        }
+    index++;
+
+}
